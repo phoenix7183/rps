718f93e9a2ea6b437dfea2cc265773c235f1f828
Vishal Singh <vishalsinghsh1728@gmail.com>
2025-08-02 02:12:43 +0530

    java.js

--- java.js
+++ java.js
@@ -6,20 +6,23 @@ function computerChoice (){
   let compOption= choices[Math.floor(Math.random()*choices.length)];
   return compOption;
 }
-console.log(computerChoice());
+
+
 
 function userChoice (){
    let userOption= prompt("choose among rock, paper, scissors","");
     let userOptCaseInsensitive= userOption.toLowerCase();
    return userOptCaseInsensitive;
 }
-console.log(userChoice());
 
 let userScore=0;
 let computerScore=0;
 
 function playRound(){
     for (let i = 0; i <= 5; i++) {
+        console.log(computerChoice());
+        console.log(userChoice());
+
         if (computerChoice()=="rock" && userChoice()=="rock") {
             return alert("draw")
            }
@@ -33,9 +36,28 @@ function playRound(){
         }  
         else if (computerChoice()=='paper'&& userChoice=="rock"){
             computerScore++;
-            return(alert("paper beats rock"));
+            return alert("paper beats rock");
         }
-        
+        else if ( computerChoice()== 'paper'&& userChoice()=='paper'){
+            return alert("it's a draw");
+        }
+        else if (computerChoice()=='paper'&& userChoice()=='scissors'){
+            userScore++;
+            return alert('scissors beat paper');
+        }
+        else if (computerChoice()=='scissors'&& userChoice()=='rock'){
+            userScore++;
+            return alert('rock beats scissors');
+        }
+        else if(computerChoice()=='scissors'&& userChoice()=='paper'){
+            computerScore++;
+            return alert('scissors beats paper');
+        }
+        else if (computerChoice()=='scissors'&& userChoice()=='scissors'){
+            return alert('it is a draw')
+        }
     }
 }
 console.log(playRound());
+console.log(userScore);
+console.log(computerScore);
