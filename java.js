3025bc4d79c9d92d7fd06e650762174b2d36ebc4
Vishal Singh <vishalsinghsh1728@gmail.com>
2025-08-02 00:42:13 +0530

    java.js

--- java.js
+++ java.js
@@ -1,1 +1,9 @@
 console.log("hello World");
+
+function computerChoice (){
+
+    let choices= ["rock", "paper", "scissors"];
+  let compOption= Math.floor(Math.random()*choices.length);
+  return compOption;
+}
+console.log(computerChoice());
