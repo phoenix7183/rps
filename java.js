b3fe35a884e13b2ccb6cc9b87f0c567c0cef85f7
Vishal Singh <vishalsinghsh1728@gmail.com>
2025-08-04 05:01:55 +0530

    error update

--- java.js
+++ java.js
@@ -4,6 +4,9 @@ const scissors=document.querySelector(".scissors");
 let choices=["rock", "paper", "scissors"];
 const div=document.querySelector(".div");
 const h1= document.querySelector('h1');
+const playerTotal= document.querySelector(".player");
+const compTotal=document.querySelector(".comp");
+
 
 let playerScore=0;
 let compScore=0;
@@ -21,17 +24,19 @@ function compChoice(){
    if (userSelection==compSelection){
     h1.textContent= "It's a tie"
     playerScore++;
-    playerScore++;
+    compScore++;
    }
    else if(userSelection=="rock" && compSelection=="scissors"||userSelection=="paper"&&compSelection=="rock"||userSelection=="scissors"&&compSelection=="paper"){
     h1.textContent= `You win! ${userSelection} beats ${compSelection}`;
     playerScore++;
     }
 else if(userSelection=="scissors" && compSelection=="rock"||userSelection=="rock"&&compSelection=="paper"||userSelection=="paper"&&compSelection=="scissors"){
-    h1.textContent= `You win! ${compSelection} beats ${userSelection}`;
+    h1.textContent= `You Loose! ${compSelection} beats ${userSelection}`;
     compScore++;
     }
 
+playerTotal.textContent=`${playerScore}`
+compTotal.textContent=`${compScore}`
 
 }
 
